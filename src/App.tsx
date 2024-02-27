139bf89df5bd37a2d79b3fa012b3be74f8346f81
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-27 17:33:36 +0100

    Add button to add todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Container } from './components/Container'
+import { Button } from './components/ui/button'
 
 function App() {
     return (
         <>
             <Container>
-                <h2>placeholder</h2>
+                <div className="border-2 w-1/4 h-fit p-4">
+                    <Button variant={'secondary'}>Add Todo</Button>
+                </div>
             </Container>
         </>
     )
