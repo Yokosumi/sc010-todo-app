9c9032133fd79dc035380152fb9f4a5201a5096f
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-04 13:58:46 +0100

    outsourced handlePost function

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -2,7 +2,7 @@ import { Container } from './components/Container'
 import { Button } from './components/ui/button'
 import { Input } from '@/components/ui/input'
 import { TodoList } from './components/TodoList'
-import axios from 'axios'
+import { handlePost } from '@/../utils/FetchData'
 import { useState } from 'react'
 
 export const TodoApp = () => {
@@ -11,25 +11,6 @@ export const TodoApp = () => {
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const todoBody = event.target.value
         setInputState(todoBody)
-    }
-
-    const handlePost = async (value: string) => {
-        try {
-            const newTodo = {
-                body: value,
-                checked: false,
-                badges: [''],
-            }
-
-            await axios.post('http://localhost:3075/todos', newTodo, {
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    'Content-Type': 'application/json',
-                },
-            })
-        } catch (e: any) {
-            throw new Error(e)
-        }
     }
 
     return (
