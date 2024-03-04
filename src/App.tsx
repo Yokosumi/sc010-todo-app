46bf66c9672dd78cb455755fe02f46644d0ae1fc
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-04 11:34:03 +0100

    Add handlePost function for posting new todos

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Container } from './components/Container'
 import { Button } from './components/ui/button'
 import { Input } from '@/components/ui/input'
 import { TodoList } from './components/TodoList'
+import { handlePost } from '../utils/FetchData'
 
 function App() {
     return (
@@ -16,7 +17,12 @@ function App() {
                     <Container className="border-2" size="md">
                         <Container direction="row">
                             <Input className="text-secondary-foreground" />
-                            <Button variant={'secondary'}>Add Todo</Button>
+                            <Button
+                                onClick={() => handlePost}
+                                variant={'secondary'}
+                            >
+                                Add Todo
+                            </Button>
                         </Container>
                         <TodoList />
                     </Container>
--- utils/AppContext.tsx
+++ utils/AppContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useState, useEffect } from 'react'
 import { Todo, AppContextType } from './types'
 import { fetchSingleEndpoint } from './FetchData'
+import axios from 'axios'
 
 const todoContext: AppContextType = {
     todoList: [],
